7be38f962c3210f907af2dc10ebd9d2b6fccda0f
Andres Valencia <andresv2@mac.com>
2019-04-03 20:18:05 -0400

    adding header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,36 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-  <div className="HeaderGroup">
-    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    <Link to="/courses">Courses</Link>
-    <Link to="/downloads">Downloads</Link>
-    <Link to="/workshops">Workshops</Link>
-    <Link to="/buy"><button>Buy</button></Link>
-  </div>
-  </div>
-)
+
+  class Header extends React.Component {
+    constructor (props) {
+      super(props) 
+
+      this.state = {
+        hasScrolled= false;
+      }
+    }
+
+    componentDidMount() {
+      window.addEventListener('scroll', this.hasScroll)
+    }
+
+    
+
+    render () {
+      return (
+        <div className="Header">
+        <div className="HeaderGroup">
+        <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy"><button>Buy</button></Link>
+      </div>
+    </div>
+      )
+    }
+  }
+
 
 export default Header
